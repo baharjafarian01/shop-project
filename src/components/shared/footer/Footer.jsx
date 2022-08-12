d956825c42d2faa9911fa56dc9aabf73daea8904
bahar <my@baharjafarian.ir>
2022-08-12 21:52:32 +0430

    footer created

---
+++ src/components/shared/footer/Footer.jsx
@@ -0,0 +1,11 @@
+import styles from "./Footer.module.css";
+
+const Footer = () => {
+  return (
+    <div className={styles.container}>
+      <span>Shopping Card Project | Bahar Jafarian</span>
+    </div>
+  );
+};
+
+export default Footer;
